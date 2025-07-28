1200e2874511836c02e6798a3dbb617d81813dce
Alberto Pingo <contact@albertopingo.com>
2025-07-28 22:16:49 +0100

    Add Nvidia drivers

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -2,4 +2,19 @@
 
 {
   networking.hostName = "desktop";
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+    modesetting.enable = true;
+
+    nvidiaSettings = true;
+
+    # Not supported by my GPU
+    open = false;
+  };
+
+
 }
--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,7 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  imports = [];
-
   networking.hostName = "laptop";
+
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+    modesetting.enable = true;
+
+    nvidiaSettings = true;
+
+    # Not supported by my GPU
+    open = false;
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      offload.enable = true;
+      sync.enable = false;
+    };
+  };
+
 }
