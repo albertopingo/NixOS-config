f3bc626eaae2db970dd6f50f3b5a92fa43ed4149
Alberto Pingo <contact@albertopingo.com>
2025-07-29 04:23:20 +0100

    Add nvidia-offload

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -24,4 +24,15 @@
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      #!/bin/bash
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '')
+  ];
+
 }
